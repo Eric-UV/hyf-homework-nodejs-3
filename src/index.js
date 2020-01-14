42f2635096315d9aa4445e154952088adcdcc2bb
Eric-UV <edelgadillo@unitedvirtualities.com>
2020-01-14 13:10:21 -0700

    im very tired

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const data = require('./data/users.json');
+//const data = require('./data/users.json');
 const app = express();
 const port = 3000;
 const fs = require('fs');
+const users = [];
 
 app.use(express.urlencoded());
 app.use(express.json());
@@ -11,33 +12,40 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 app.get('/users', (req, res) => {
-    res.send(data);
+    res.send(users);
 });
 app.get('/user/:id', (req, res) => {
-    let users = JSON.parse(JSON.stringify(data));    
+    //let users = JSON.parse(JSON.stringify(data));    
     let user = users.find(user => user.id == req.params.id);
     res.json(user);
 });
 app.post('/user', (req, res) => {    
-    console.log(req.body);
+    /*console.log(req.body);
     let user = {
         id: req.body.id,
         name: req.body.name,
         email: req.body.email
-    };
+    };*/
 
-    let users = JSON.parse(JSON.stringify(data));
-    users.push(user);
-    let allUsers = JSON.stringify(users);
-    fs.writeFileSync(__dirname + '/data/users.json', allUsers);
+    let users = []; //JSON.parse(JSON.stringify(data));
+    users.push({ id: 0 });
+    //let allUsers = JSON.stringify(users);
+    //fs.writeFileSync(__dirname + '/data/users.json', allUsers);
         
     res.json(users);
 });
 
 app.delete('/user', (req, res) => {
-    res.status(202).json({
-        ok: true
-    });
+    if(users.length > 0){
+        users = [];
+        res.status(202).json({
+            ok: true
+        }) 
+    }else{
+        res.status(204).json({
+            ok: true
+        });
+    }
 });
 
 app.listen(port, err => {
